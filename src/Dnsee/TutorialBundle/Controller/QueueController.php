20ae3bdb6458b4165b2681f7f5f0bf3a38e4b5cf
Matteo Biagetti <m.biagetti@dnsee.com>
2013-03-21 12:57:43 +0100

    fix homepage link

--- src/Dnsee/TutorialBundle/Controller/QueueController.php
+++ src/Dnsee/TutorialBundle/Controller/QueueController.php
@@ -9,33 +9,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class QueueController extends Controller
 {
-    /**
-     * @Route("/produce/{subject}/{message}/{tot}", defaults={"tot" = 1})
-     * @Template()
-     */
-    public function produceAction($subject,$message,$tot)
-    {
-        $msg=new Message();
-        $msg->setBody($message);
-        $msg->setSubject($subject);
-
-        /**
-         *  Puoi inviare messaggi di tipo XML, Json o text plain semplicemente modificando il content type
-         * della coda, insomma qualsiasi formato che sia serializzabile, ovviamente anche oggetti.
-         *
-            $this->get('old_sound_rabbit_mq.custom_message_producer')->setContentType('application/json');
-
-         * il default è text/plain
-         */
-
-        for($i=0;$i<$tot;$i++)
-            $this->get('old_sound_rabbit_mq.custom_message_producer')->publish(serialize($msg));
-
-        return array('msg' => $msg, 'tot'=>$tot);
-    }
 
     /**
-     * @Route("/rouningKey/{subject}/{message}/{key}/{tot}", defaults={"tot" = 1})
+     * @Route("/rouningKey/{subject}/{message}/{key}/{tot}", name ="_key", defaults={"tot" = 1})
      * @Template()
      */
     public function keyAction($subject,$message,$key,$tot)
